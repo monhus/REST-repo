fa15c36c9f6266c467132673a5722b1909cb8497
Monika Husar <husarmonika@gmail.com>
2020-03-27 21:45:03 +0100

    zmiana w main na jestem klonem

--- src/pl/java/Main.java
+++ src/pl/java/Main.java
@@ -3,6 +3,6 @@ package pl.java;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Umieść mnie na GitHub'ie");
+        System.out.println("Jestem klonem");
     }
 }
